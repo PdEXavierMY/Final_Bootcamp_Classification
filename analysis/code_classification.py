2a54b2f568d4988c065bc68a05accf410cb49a67
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-11-28 11:07:34 +0100

    .

--- analysis/code_classification.py
+++ analysis/code_classification.py
@@ -5,11 +5,7 @@ import numpy as np
 import pylab as plt
 import seaborn as sns
 
-from sklearn.linear_model import LogisticRegression            # logistic regression model   
-from sklearn.preprocessing import MinMaxScaler                 # standarized
-from sklearn.preprocessing import LabelEncoder                 # Para codificar nuestra variable a predecir
-
-from sklearn.model_selection import train_test_split     # split data into train and test sets
-
 import warnings
 warnings.filterwarnings('ignore')
+
+data = pd.read_csv('./data/creditcardmarketing.csv')
