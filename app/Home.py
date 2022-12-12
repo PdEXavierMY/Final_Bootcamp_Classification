1ce3b05b41dad6f1b3ecead7b7798c70bbaf1062
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-12-12 22:00:31 +0100

    .

--- app/Home.py
+++ app/Home.py
@@ -6,7 +6,15 @@ st.set_page_config(
 
 st.title("😈 Proyecto Final")
 
+dict = {
+    "Javier Míguelez": "https://github.com/Xavitheforce",
+    "Juan Medina": "https://github.com/jmedina28",
+    "Diego de Santos": "https://github.com/Diegodesantos1"
+}
 st.write('''👁️👄👁️
     Tras la realización del curso de Data Science, nos hemos dispuesto a realizar un proyecto final.
     El proyecto consiste en la creación de una aplicación web que muestre los resultados de dos modelos, uno de regresión y otro de clasificación.
-    ''')
+    Los autores de este proyecto son los siguientes:''')
+
+for key, value in dict.items():
+    st.write(f"👉 {key} 🥶 {value}")
