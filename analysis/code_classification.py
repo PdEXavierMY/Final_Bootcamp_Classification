d981fd4830de9de6247bf01b1b32744ad5c5b9d2
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-12-12 10:21:09 +0100

    .

--- analysis/code_classification.py
+++ analysis/code_classification.py
@@ -14,12 +14,13 @@ warnings.filterwarnings('ignore')
 
 # Importamos los datos
 data = pd.read_csv('./data/creditcardmarketing.csv')
+print(data) #vemos que se ha importado correctamente
+print(data.head(10), "\n") #vemos las 10 primeras filas del dataset
 
 #sacamso la información de los datos para revisar si necesitamos hacer algún ajuste a nuestros dataset
 #analizamos el dataset
 
-print(data.head(), "\n")
-print("Estructura de los datos: ", data.shape, "\n")
+print("Estructura de los datos: ", data.shape, "\n") #vemos las filas y columnas que tenemos
 print(data.describe(), "\n")
 print(data.info(), "\n")
 print(data.isnull().sum(), "\n")
@@ -28,4 +29,12 @@ print(data.isnull().sum(), "\n")
 #Hay varias columnas con varos nulos que analizaremos más adelante, y varias columnas no numericas
 #que también modificaremos.
 
-#data cleaning
+#primero miraremos la correlación de las variables numéricas
+
+lista = data.select_dtypes(include=['int64', 'float64']).columns; print(lista)
+corr = data[lista].corr()
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr, annot=True)
+plt.show()
+
+#Vemos que no hay ninguna variable que esté muy correlacionada con otra, por lo que no es necesario eliminar ninguna columna basandose en este criterio.
